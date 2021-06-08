0b05c83255c495ba355fc48cad9b30b2d60d35ee
Earl Panganiban <earl@quadrivium.xyz>
2021-06-08 10:58:17 +0800

    Added structs lesson code

---
+++ 06-structs/shapes.go
@@ -0,0 +1,37 @@
+package main
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func (rectangle Rectangle) Perimeter() float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+func (circle Circle) Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
+}
+
+func (triangle Triangle) Area() float64 {
+	return 0.5 * triangle.Base * triangle.Height
+}
