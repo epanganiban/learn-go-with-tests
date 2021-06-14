9a9bc9e9da660342bd2124d499aaefc8fe87f517
Earl Panganiban <earl@quadrivium.xyz>
2021-06-14 16:28:40 +0800

    Refactored to include maps

--- 13-reflection/reflection.go
+++ 13-reflection/reflection.go
@@ -15,21 +15,24 @@ func getValue(x interface{}) reflect.Value {
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value
-
-	switch val.Kind() {
-	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field
-	case reflect.Slice, reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index
-	case reflect.String:
-		fn(val.String())
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
 	}
 
-	for i := 0; i < numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
 	}
 }
