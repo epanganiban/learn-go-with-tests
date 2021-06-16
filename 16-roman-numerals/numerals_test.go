a3969554a6e4cec6747fb074bfc927bbd5efa7e7
Earl Panganiban <earl@quadrivium.xyz>
2021-06-16 09:54:09 +0800

    Refactored to use string builder

--- 16-roman-numerals/numerals_test.go
+++ 16-roman-numerals/numerals_test.go
@@ -3,22 +3,23 @@ package main
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+	}
 
-		if got != want {
-			t.Errorf("want %q but got %q", want, got)
-		}
-	})
-
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("want %q but got %q", want, got)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("want %q but got %q", test.Want, got)
+			}
+		})
+	}
 
 }
