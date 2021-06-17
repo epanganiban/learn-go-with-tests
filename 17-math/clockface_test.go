0104b63046d17b642181f8c98f5ad5643830df63
Earl Panganiban <earl@quadrivium.xyz>
2021-06-17 10:07:15 +0800

    Added test and function for seconds in radians

--- 17-math/clockface_test.go
+++ 17-math/clockface_test.go
@@ -1,19 +1,58 @@
-package clockface_test
+package clockface
 
 import (
+	"math"
 	"testing"
 	"time"
-
-	"clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+func TestSecondsInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("want %v but got %v", want, got)
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("want %v radians but got %v", c.angle, got)
+			}
+		})
 	}
 }
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
+
+// func TestSecondHandAtMidnight(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+// 	want := clockface.Point{X: 150, Y: 150 - 90}
+// 	got := clockface.SecondHand(tm)
+
+// 	if got != want {
+// 		t.Errorf("want %v but got %v", want, got)
+// 	}
+// }
+
+// func TestSecondHandAt30Seconds(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+// 	want := clockface.Point{X: 150, Y: 150 + 90}
+// 	got := clockface.SecondHand(tm)
+
+// 	if got != want {
+// 		t.Errorf("want %v but got %v", want, got)
+// 	}
+// }
