9316599ef2c3682ddfe6eac8d17b4190c7ce6ad1
Earl Panganiban <earl@quadrivium.xyz>
2021-06-17 11:55:11 +0800

    Modified hour hand to move in smaller increments

--- 17-math/clockface.go
+++ 17-math/clockface.go
@@ -12,7 +12,7 @@ type Point struct {
 }
 
 func hoursInRadians(t time.Time) float64 {
-	return (math.Pi / (6 / float64(t.Hour()%12)))
+	return (minutesInRadians(t) / 12) + (math.Pi / (6 / float64(t.Hour()%12)))
 }
 
 func minutesInRadians(t time.Time) float64 {
