29a366823dd4394828405ff7b55b07f787f337d6
Earl Panganiban <earl@quadrivium.xyz>
2021-06-17 11:22:14 +0800

    Added function for getting minute hand vector

--- 17-math/clockface.go
+++ 17-math/clockface.go
@@ -15,12 +15,19 @@ func minutesInRadians(t time.Time) float64 {
 	return (secondsInRadians(t) / 60) + (math.Pi / (30 / (float64(t.Minute()))))
 }
 
+func minuteHandPoint(t time.Time) Point {
+	return angleToPoint(minutesInRadians(t))
+}
+
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
 
 func secondHandPoint(t time.Time) Point {
-	angle := secondsInRadians(t)
+	return angleToPoint(secondsInRadians(t))
+}
+
+func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 
