517b32583781c04a5b0b7e26ae5ddeac658fd608
Earl Panganiban <earl@quadrivium.xyz>
2021-06-18 09:55:40 +0800

    Initial commit of starter code after refactoring to use fs.FS

---
+++ 18-reading-files/blogposts_test.go
@@ -0,0 +1,20 @@
+package blogposts_test
+
+import (
+	"blogposts"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts := blogposts.NewPostsFromFS(fs)
+
+	if len(posts) != len(fs) {
+		t.Errorf("wanted %d posts but got %d", len(fs), len(posts))
+	}
+}
