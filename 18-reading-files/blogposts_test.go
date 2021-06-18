9d044572ff4d2f6c6042180778a3abbb241b5df4
Earl Panganiban <earl@quadrivium.xyz>
2021-06-18 10:18:31 +0800

    Added error handling and StubFailing testdouble

--- 18-reading-files/blogposts_test.go
+++ 18-reading-files/blogposts_test.go
@@ -2,19 +2,41 @@ package blogposts_test
 
 import (
 	"blogposts"
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 )
 
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
+}
+
 func TestNewBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
-	}
+	t.Run("successful load", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"hello world.md":  {Data: []byte("hi")},
+			"hello-world2.md": {Data: []byte("hola")},
+		}
 
-	posts := blogposts.NewPostsFromFS(fs)
+		posts, err := blogposts.NewPostsFromFS(fs)
 
-	if len(posts) != len(fs) {
-		t.Errorf("wanted %d posts but got %d", len(fs), len(posts))
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(posts) != len(fs) {
+			t.Errorf("wanted %d posts but got %d", len(fs), len(posts))
+		}
+	})
+
+	t.Run("load failure", func(t *testing.T) {
+		_, err := blogposts.NewPostsFromFS(StubFailingFS{})
+		if err == nil {
+			t.Fatal("expected an error but got nil")
+		}
+	})
 }
