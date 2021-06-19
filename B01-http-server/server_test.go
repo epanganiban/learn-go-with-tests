8c421add8d8ff739155d77a7d658da65ff8cbbaf
Earl Panganiban <earl@quadrivium.xyz>
2021-06-19 15:00:17 +0800

    Added test helpers to facilitate DRY

--- B01-http-server/server_test.go
+++ B01-http-server/server_test.go
@@ -1,37 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("want %q but got %q", want, got)
+	}
+}
+
 func TestGETPlayers(t *testing.T) {
-	t.Run("", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+	t.Run("return Pepper's score", func(t *testing.T) {
+		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("want %q but got %q", want, got)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 	})
 
-	t.Run("", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+	t.Run("return Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "10"
-
-		if got != want {
-			t.Errorf("want %q but got %q", want, got)
-		}
+		assertResponseBody(t, response.Body.String(), "10")
 	})
 }
